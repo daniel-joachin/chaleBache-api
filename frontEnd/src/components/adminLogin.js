416dbd0d1fbc9a513ebd882a7a8c0f782c1b1d2f
VictorGuido <30873840+VictorGuido@users.noreply.github.com>
2021-05-13 19:55:39 -0500

    mainStyle and LoginStyle

--- frontEnd/src/components/adminLogin.js
+++ frontEnd/src/components/adminLogin.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import './styles/adminLogin.css'
 
 export default class AdminLogin extends Component {
     render() {
         return (
-            <div>
-                <div className= 'login-box'>
-                    <h1>Hello</h1>
+            <div >
+                <div className='loginApp'></div>
+                <div id='formLogin'>
+                    <form className='box'>
+                        <h1>Inicio de Sesión</h1>
+                        <input id="loginI" type="text" name="" placeholder="USUARIO / CORREO"></input>
+                        <input id="loginI" type="password" name="" placeholder="CONTRASEÑA" ></input>
+                        <input type="button" name="" value="Iniciar" onclick="sayHola()"></input>
+                    </form>
+                
                 </div>
             </div>
         )
--- frontEnd/src/components/bacheInfo.js
+++ frontEnd/src/components/bacheInfo.js
@@ -10,10 +10,25 @@ function BacheInfo () {
             <ul>
                 {/* <li>{bache.name}</li> */}
                 <li id="bacheName">{bache.name}</li>
-                <li>Fecha de nacimiento:  {bache.firstIncident?.toString().substring(0,10)}</li>
-                <li>Ultimo incidente: {bache.lastIncident?.toString().substring(0,10)}</li>
-                <li>Numero de incidentes: {bache.numIncidents}</li>
+                <li id="fechaB">Fecha de nacimiento:  {bache.firstIncident?.toString().substring(0,10)}</li>
+                <li id="ultiB">Ultimo incidente: {bache.lastIncident?.toString().substring(0,10)}</li>
+                <li id="numB">Numero de incidentes: {bache.numIncidents}</li>
             </ul>
+
+            <div id="bacheLogs" style={{'justifyContent':'end', 'position':'absolute','bottom':'0'}}>
+                <ul>
+                    {/* <li>{bache.name}</li> */}
+                    <li id="bacheName">Actualizaciones</li>
+                    <li id="fechaB">Noticia #1   </li>
+                    <li id="ultiB">Noticia #2 </li>
+                    <li id="numB">Noticia #3 </li>
+                    <li id="ultiB">Noticia #4 </li>
+                    <li id="numB">Noticia #5 </li>
+                </ul>
+            </div>
+            
+
+
         </div>
     )
 }
